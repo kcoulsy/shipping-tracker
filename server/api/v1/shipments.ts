9c49776b385fc333337f4f5ef910d6890acd7bed
Kristian Coulson <kristiancoulson@gmail.com>
2024-07-04 07:47:21 +0100

    feat: post shipment

--- server/api/v1/shipments.ts
+++ server/api/v1/shipments.ts
@@ -1,24 +1,32 @@
+import { zValidator } from "@hono/zod-validator";
 import { Hono } from "hono";
+import { z } from "zod";
 
-interface Shipment {
-  id: number;
-  contents: string;
-  dateShipped: Date;
-  status: "not-shipped" | "shipped" | "delivered";
-  name: string;
-  addressLine1: string;
-  addressLine2: string;
-  city: string;
-  postcode: string;
-  courier: string;
-  trackingNumber: string;
-}
+const shipmentSchema = z.object({
+  id: z.number(),
+  contents: z.string(),
+  dateShipped: z.string(),
+  status: z.union([
+    z.literal("not-shipped"),
+    z.literal("shipped"),
+    z.literal("delivered"),
+  ]),
+  name: z.string(),
+  addressLine1: z.string(),
+  addressLine2: z.string(),
+  city: z.string(),
+  postcode: z.string(),
+  courier: z.string(),
+  trackingNumber: z.string(),
+});
+
+type Shipment = z.infer<typeof shipmentSchema>;
 
 const shipments: Shipment[] = [
   {
     id: 1,
     contents: "Books",
-    dateShipped: new Date(),
+    dateShipped: new Date().toISOString(),
     status: "shipped",
     name: "John Doe",
     addressLine1: "123 Fake St",
@@ -31,7 +39,7 @@ const shipments: Shipment[] = [
   {
     id: 2,
     contents: "Electronics",
-    dateShipped: new Date(),
+    dateShipped: new Date().toISOString(),
     status: "not-shipped",
     name: "Jane Doe",
     addressLine1: "456 Fake St",
@@ -45,8 +53,24 @@ const shipments: Shipment[] = [
 
 const app = new Hono();
 
-app.get("/", (c) => {
-  return c.json(shipments);
-});
+app
+  .get("/", (c) => {
+    return c.json(shipments);
+  })
+  .post(
+    "/",
+    zValidator(
+      "json",
+      shipmentSchema.omit({
+        id: true,
+      })
+    ),
+    (c) => {
+      const shipment = c.req.valid("json");
+      const id = shipments.length + 1;
+      shipments.push({ ...shipment, id });
+      return c.json({ id });
+    }
+  );
 
 export default app;
